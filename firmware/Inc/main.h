a502d3fe56f484eec3909823ef80bad9aedf974d
Nils Hasenbanck <nils@hasenbanck.de>
2018-09-18 10:47:15 +0200

    Redesigned LSE layout and re-routed some traces
    Also switched BTN3 and BTN4 pins for easier routing.

--- firmware/Inc/main.h
+++ firmware/Inc/main.h
@@ -22,9 +22,9 @@
 #define BTN1_GPIO_Port GPIOB
 #define BTN2_Pin GPIO_PIN_4
 #define BTN2_GPIO_Port GPIOB
-#define BTN3_Pin GPIO_PIN_6
+#define BTN3_Pin GPIO_PIN_7
 #define BTN3_GPIO_Port GPIOB
-#define BTN4_Pin GPIO_PIN_7
+#define BTN4_Pin GPIO_PIN_6
 #define BTN4_GPIO_Port GPIOB
 
 void _Error_Handler(char *, int);
