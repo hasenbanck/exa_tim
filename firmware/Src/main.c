9231bdc18d805f712a0a0f600cee602acb5aea24
Nils Hasenbanck <nils@hasenbanck.de>
2018-09-18 11:10:06 +0200

    Add SVD file and lowered LSE drive current
    I already use a 6pf crystall and still can't get it to run
    under RCC_LSEDRIVE_LOW or RCC_LSEDRIVE_MEDIUMLOW.

--- firmware/Src/main.c
+++ firmware/Src/main.c
@@ -58,7 +58,7 @@ void SystemClock_Config(void) {
 
   /** Configure LSE Drive Capability */
   HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_HIGH);
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_MEDIUMHIGH);
 
   /** Initializes the CPU, AHB and APB busses clocks */
   RCC_OscInitStruct.OscillatorType =
